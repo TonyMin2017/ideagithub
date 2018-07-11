4938a31930347e1a2c6ae850b76c4a9e2790a6c3
TonyMin <1302981271@qq.com>
2018-07-11 14:27:55 +0800

    添加懒汉式单例的设计模式代码；

--- src/main/java/com/sz/yb/github/SingleTon.java
+++ src/main/java/com/sz/yb/github/SingleTon.java
@@ -4,9 +4,11 @@ package com.sz.yb.github;
  * Created by tony on 2018/7/11.
  */
 public class SingleTon {
-    //Java设计模式之闪单例模式：饿汉式；线程安全；
-    private static SingleTon singleTon = new SingleTon();
+    private static SingleTon singleTon = null;  //懒汉式单例设计模式；
     public static SingleTon getSingleTon(){
+        if(singleTon==null){
+            singleTon = new SingleTon();
+        }
         return singleTon;
     }
     private SingleTon(){
